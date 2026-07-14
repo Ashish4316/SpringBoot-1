33736d5455471ae0166eb81ecbf96c1cc105a66e
Ashish <yadavashish72408@gmail.com>
2026-07-14 15:32:58 +0530

    create student repository

---
+++ src/main/java/com/example/demo/StudentServer/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.StudentServer;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+    public Student save(Student student) {
+        System.out.println("Student Information saved");
+        return student;
+    }
+}
