fa370f8c286bf4d4d849890a43b0c2d94bf08ffd
Ashish <yadavashish72408@gmail.com>
2026-07-14 14:36:17 +0530

    create student server

---
+++ src/main/java/com/example/demo/StudentServer/StudentServer.java
@@ -0,0 +1,17 @@
+package com.example.demo.StudentServer;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentServer {
+    @PostMapping("/create")
+    public String storeStudent(@RequestBody Student student) {
+        int id = student.getID();
+        String name = student.getName();
+        int age = student.getAge();
+        String department = student.getDepartment();
+        return "id: " + id + " " + "name: " + name + " " + "age: " + age + " " + "department: " + department;
+    }
+}
