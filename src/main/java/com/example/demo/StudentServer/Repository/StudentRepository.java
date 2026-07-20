f11551be65bfa46c30f413c20d46e332b03c29ae
Ashish <yadavashish72408@gmail.com>
2026-07-20 12:55:56 +0530

    Refactor service to use DTO

---
+++ src/main/java/com/example/demo/StudentServer/Repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.StudentServer.Repository;
+
+import com.example.demo.StudentServer.Entiity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+}
---
+++ src/main/java/com/example/demo/StudentServer/Service/StudentService.java
@@ -0,0 +1,79 @@
+package com.example.demo.StudentServer.Service;
+
+
+import com.example.demo.StudentServer.DTO.CreateStudentRequestDTO;
+import com.example.demo.StudentServer.DTO.CreateStudentResponseDTO;
+import com.example.demo.StudentServer.Entiity.Student;
+import com.example.demo.StudentServer.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class StudentService {
+    StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public CreateStudentResponseDTO studentValidate(CreateStudentRequestDTO createStudentRequestDTO) {
+
+        Student student = mapToStudent(createStudentRequestDTO);
+        return mapToResponseDTO(student);
+    }
+
+    public Student getStudentById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public Student studentUpdate(int id, Student student) {
+
+        Student result = studentRepository.findById(id).orElse(null);
+
+        if (result == null) {
+            return null;
+        }
+
+        result.setName(student.getName());
+        result.setAge(student.getAge());
+        result.setDepartment(student.getDepartment());
+        result.setUpdatedAt(LocalDateTime.now());
+
+        return studentRepository.save(result);
+    }
+
+    public Student deleteStudent(int id) {
+        Student result = studentRepository.findById(id).orElse(null);
+        if(result == null) {
+            return null;
+        }
+        studentRepository.delete(result);
+        return result;
+    }
+
+    private Student mapToStudent(CreateStudentRequestDTO createStudentRequestDTO) {
+        Student student = new Student();
+
+        student.setName(createStudentRequestDTO.getName());
+        student.setAge(createStudentRequestDTO.getAge());
+        student.setDepartment(createStudentRequestDTO.getDepartment());
+        student.setCreatedAt(LocalDateTime.now());
+        student.setUpdatedAt(LocalDateTime.now());
+
+        return student;
+    }
+
+    private CreateStudentResponseDTO mapToResponseDTO(Student student) {
+        CreateStudentResponseDTO createStudentResponseDTO = new CreateStudentResponseDTO();
+        createStudentResponseDTO.setId(student.getId());
+        createStudentResponseDTO.setName(student.getName());
+        createStudentResponseDTO.setAge(student.getAge());
+        createStudentResponseDTO.setDepartment(student.getDepartment());
+
+        return createStudentResponseDTO;
+
+    }
+}
