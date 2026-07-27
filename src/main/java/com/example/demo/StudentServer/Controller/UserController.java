c46555f7cf6856b8691856aab5cf80691ffea09c
Ashish <yadavashish72408@gmail.com>
2026-07-27 15:27:37 +0530

    add User Entity,Controller,Repository and Service

---
+++ src/main/java/com/example/demo/StudentServer/Controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.demo.StudentServer.Controller;
+
+import com.example.demo.StudentServer.Entiity.User;
+import com.example.demo.StudentServer.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        return service.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAll(){
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id){
+
+        return service.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User update(
+            @PathVariable Long id,
+            @RequestBody User user){
+
+        return service.update(id,user);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id){
+
+        service.delete(id);
+
+        return "Deleted";
+    }
+}
